1e34d41e2ea11b1ae461fe2a59fdf57c85d9f45c
Fizahra <fitriazzahra3@gmail.com>
2023-06-10 21:45:19 +0700

    slicing ui article

---
+++ app/src/main/java/com/example/happyvet/ui/activity/DetailArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happyvet.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.happyvet.R
+
+class DetailArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_article)
+    }
+}
